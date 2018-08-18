648487e51135013da22093119d5651f410720b64
CoderGirlBre <41300229+CoderGirlBre@users.noreply.github.com>
2018-08-18 15:41:13 -0500

    Add files via upload

--- DiceGame/Program.cs
+++ DiceGame/Program.cs
@@ -6,7 +6,52 @@ namespace DiceGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Dice Game: ".ToUpper());
+            Console.Write("Who will get to 20 first? (You VS Computer)");
+            Console.ReadLine();
+
+            
         }
+
+        private static void DiceRolling()
+        {
+            
+            int sum = 0;
+            bool incorrect = true;
+
+            do
+            {
+                Console.WriteLine("Press enter to roll the dice!");
+                Console.ReadLine();
+
+                Random dice = new Random();
+                int rolled = dice.Next(2, 13);
+                sum += rolled;
+
+                Console.WriteLine("Dice: " + rolled);
+                Console.WriteLine("Total: " + sum);
+
+                if (sum == 20)
+                {
+                    Console.WriteLine("WINNER!");
+                    Console.ReadLine();
+                    incorrect = false;
+                }
+                else if (sum > 20)
+                {
+                    Console.WriteLine("WINNER!");
+                    Console.ReadLine();
+                    incorrect = false;
+                }
+                else
+                incorrect = true;
+               
+        
+            } while (incorrect);
+        }
+       
+
+        
+       
     }
 }
